715b41006796d4a4707b79b7c840d6129a97a32a
MarkHiarley <hiarleycostamarley@gmail.com>
2024-08-21 20:56:08 -0300

    bottom js moon

--- js/index.js
+++ js/index.js
@@ -4,7 +4,11 @@ console.log(botaoAlterarTema);
 const body = document.querySelector('body'); 
 
 botaoAlterarTema.addEventListener("click", () => {
-  body.classList.add("modo-escuro");
+  body.classList.toggle("modo-escuro");
 
+  if (body.classList.contains("modo-escuro")) {
+    botaoAlterarTema.querySelector('img').src = "./src/imagens/lua.webp"; // Ícone para o modo escuro
+  } else {
+    botaoAlterarTema.querySelector('img').src = "./src/imagens/sol.png"; // Ícone para o modo claro
+  }
 });
-
